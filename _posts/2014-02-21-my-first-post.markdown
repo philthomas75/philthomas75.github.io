a1daded18e0409fcb677fabe96eeefe936efc962
philthomas75 <plainresponsive@gmail.com>
2015-06-05 23:00:59 +0100

    final

---
+++ _posts/2014-02-21-my-first-post.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "This is my first blog post"
+date:   2014-02-21 21:18
+categories: jekyll update
+---
+
+Can you read this?
---
+++ config.rb
@@ -0,0 +1,29 @@
+# Require any additional compass plugins here.
+# require "/library/Ruby/Gems/1.8/gems/susy-1.0.8/lib/susy.rb"
+# require "/library/Ruby/Gems/1.8/gems/animate-0.2.beta.0/lib/animate.rb"
+require "susy"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ""
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+sass_options = {:debug_info => true}
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
